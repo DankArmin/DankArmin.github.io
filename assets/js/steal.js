0540e448c1223ec861699b67697710260e25b9d8
DankArmin <armangaspy@gmail.com>
2022-11-29 15:43:27 +0100

    add

--- assets/js/steal.js
+++ assets/js/steal.js
@@ -1,3 +1,9 @@
-document.cookie = "token=bruh"
+const domain = 'http://preben.net';
+const popUp = window.open(domain + '/cors.php');
 
-console.log(document.cookie);
+setTimeout(function() {
+
+    const cookie = popUp.cookie;
+    console.log(cookie);
+
+}, 1000);
